21b548d8262313086a3972328b7c67200421c917
Joel Cabatay <ascii274@gmail.com>
2022-09-21 02:01:51 +0200

    Initial commit

---
+++ src/main/java/com/ascii274/talentsquad2/TalentSquadBackend2Application.java
@@ -0,0 +1,13 @@
+package com.ascii274.talentsquad2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentSquadBackend2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentSquadBackend2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/ascii274/talentsquad2/TalentSquadBackend2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ascii274.talentsquad2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalentSquadBackend2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
